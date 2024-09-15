00fa7905ef6138a02d52d4b007e45c292900e61c
Ratmotor99 <ratmotor99@yahoo.com>
2024-09-15 01:10:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,55 +3,33 @@ import yfinance as yf
 import pandas as pd
 
 # Set the page title
-st.title("Top Dividend Stock Selector")
+st.title("Top Dividend Stock Selector (Testing with 10 Stocks)")
 
-# Step 1: Fetch S&P 500 stock symbols from Wikipedia
-@st.cache
-def get_sp500_stocks():
-    url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
-    sp500_table = pd.read_html(url, header=0)[0]
-    return sp500_table['Symbol'].tolist()
+# Step 1: Define a list of 10 dividend-paying stocks for testing
+dividend_stocks = ['AAPL', 'MSFT', 'KO', 'PEP', 'T', 'VZ', 'JNJ', 'PG', 'XOM', 'CVX']
 
-sp500_stocks = get_sp500_stocks()
+# Step 2: Input for selecting the number of top dividend stocks to display
+num_top_stocks = st.number_input("How many top dividend stocks to display?", min_value=1, max_value=10, value=5)
 
-# Step 2: Predefined ETF symbols
-etfs = ['SPY', 'IVV', 'VOO', 'QQQ', 'DIA', 'IWM', 'XLF', 'XLK', 'VTI', 'AGG']  # This can be expanded
-
-# Step 3: Add checkboxes for S&P 500 and ETFs
-include_sp500 = st.checkbox("Include S&P 500 Stocks", value=True)
-include_etfs = st.checkbox("Include ETFs", value=True)
-
-# Step 4: Create a combined stock list based on user selection
-stock_list = []
-if include_sp500:
-    stock_list.extend(sp500_stocks)
-if include_etfs:
-    stock_list.extend(etfs)
-
-# Step 5: Input for selecting the number of top dividend stocks to display
-num_top_stocks = st.number_input("How many top dividend stocks to display?", min_value=1, max_value=len(stock_list), value=5)
-
-# Fetch stock data and store in a list
+# Step 3: Fetch stock data for the predefined list of dividend stocks
 stock_data_list = []
 
-if stock_list:
-    for symbol in stock_list:
-        stock = yf.Ticker(symbol)
-        info = stock.info
+for symbol in dividend_stocks:
+    stock = yf.Ticker(symbol)
+    info = stock.info
 
-        # Check if the stock pays dividends (non-zero dividend yield)
-        dividend_yield = info.get('dividendYield', 0)
-        
-        if dividend_yield:  # Only include stocks with non-zero dividend yield
-            # Add stock data to the list
-            stock_data = {
-                'Symbol': symbol,
-                'Price': info.get('currentPrice', 'N/A'),
-                'Dividend Yield': dividend_yield,
-                'Dividend per Share': info.get('dividendRate', 'N/A')
-            }
-            
-            stock_data_list.append(stock_data)
+    # Only include stocks with non-zero dividend yield
+    dividend_yield = info.get('dividendYield', 0)
+
+    if dividend_yield:  # If dividend yield exists
+        # Add stock data to the list
+        stock_data = {
+            'Symbol': symbol,
+            'Price': info.get('currentPrice', 'N/A'),
+            'Dividend Yield': dividend_yield,
+            'Dividend per Share': info.get('dividendRate', 'N/A')
+        }
+        stock_data_list.append(stock_data)
 
 # Convert to DataFrame
 stock_data_df = pd.DataFrame(stock_data_list)
@@ -59,13 +37,13 @@ stock_data_df = pd.DataFrame(stock_data_list)
 # Sort by dividend yield (highest to lowest)
 sorted_stocks = stock_data_df.sort_values(by='Dividend Yield', ascending=False)
 
-# Step 6: Display the top N dividend stocks as specified by the user
+# Step 4: Display the top N dividend stocks as specified by the user
 top_stocks = sorted_stocks.head(num_top_stocks)
 
 st.write(f"### Top {num_top_stocks} Dividend Stocks")
 st.dataframe(top_stocks[['Symbol', 'Price', 'Dividend Yield', 'Dividend per Share']])
 
-# Step 7: Display the chart for each selected stock
+# Step 5: Display the chart for each selected stock
 for index, row in top_stocks.iterrows():
     symbol = row['Symbol']
     stock = yf.Ticker(symbol)
